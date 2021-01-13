8fb34078531319b4e7fcc8f64ce83da4ed26ab0a
Ahmed Soliman <ahmedsoliman@goudataxi.se>
2021-01-13 15:58:49 +0100

    changed startup process

---
+++ Unleased/Utilities/AdyenEncrypt.cs
@@ -0,0 +1,21 @@
+﻿using Jint;
+using System;
+
+namespace UnleashedAIO.Unleased.Utilities
+{
+    class AdyenEncrypt
+    {
+        public static string EncryptData()
+        {
+
+             var add = new Engine()
+            .Execute("function add(a, b) { return a + b; }")
+            .GetValue("add")
+            ;
+
+            add.Invoke(1, 2); // -> 3
+            Console.WriteLine(add);
+            return add.ToString();
+        }
+    }
+}
