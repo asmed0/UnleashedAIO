53dfa9e84c98838e4cc5d722218981531b4e6e43
Ahmed Soliman <ahmedsoliman@goudataxi.se>
2021-01-27 00:55:24 +0100

    working on ftl module

--- goTLS/goTLS.go
+++ goTLS/goTLS.go
@@ -14,19 +14,65 @@ import (
 func main() {}
 
 type returnStruct struct {
-	Cookies []string `json:"cookies"`
-	Body    string   `json:"body"`
-	Status  int      `json:"status"`
+	Cookies     []string `json:"cookies"`
+	Body        string   `json:"body"`
+	Status      int      `json:"status"`
+	SentBody    string   `json:"sentbody"`
+	SentUrl     string   `json:"senturl"`
+	SentHeaders []string `json:"sentheaders"`
 }
-
-func headSet(req *http.Request,headerList string[]){
-	headerAA := strings.Split(headerList[i], ",,")
-	req.Header.Set(headerAA[0], headerAA[1])
-	wg.Done()
-}
-
 
 //export getRequest
+func getRequest(urlRaw *C.char, headersRaw *C.char, proxyRaw *C.char) *C.char {
+
+	url := string(C.GoString(urlRaw))
+	headers := string(C.GoString(headersRaw))
+	proxy := string(C.GoString(proxyRaw))
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return C.CString("get error")
+	}
+
+	headerList := strings.Split(headers, "=|=|=")
+	for i := 0; i < len(headerList); i++ {
+		headerAA := strings.Split(headerList[i], ",,")
+		req.Header.Set(headerAA[0], headerAA[1])
+		//req.Header[headerAA[0]] = []string{headerAA[1]}
+	}
+
+	client, err := cclient.NewClient(tls.HelloChrome_Auto, proxy)
+	if err != nil {
+		return C.CString("newClient error")
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return C.CString("do error")
+	}
+
+	dataBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return C.CString("nil error")
+	}
+
+	var cookieCollect []string
+	for _, cookie := range resp.Cookies() {
+		cookieCollect = append(cookieCollect, cookie.Name+"="+cookie.Value)
+	}
+
+	bytes, err := json.Marshal(returnStruct{
+		Cookies: cookieCollect,
+		Body:    string(dataBody),
+		Status:  resp.StatusCode,
+	})
+	if err != nil {
+		return C.CString("error")
+	}
+
+	resp.Body.Close()
+	return C.CString(string(bytes))
+}
 
 //export postRequest
 func postRequest(urlRaw *C.char, headersRaw *C.char, bodyRaw *C.char, proxyRaw *C.char) *C.char {
@@ -74,9 +120,12 @@ func postRequest(urlRaw *C.char, headersRaw *C.char, bodyRaw *C.char, proxyRaw *
 	}
 
 	bytes, err := json.Marshal(returnStruct{
-		Cookies: cookieCollect,
-		Body:    string(dataBody),
-		Status:  resp.StatusCode,
+		Cookies:     cookieCollect,
+		Body:        string(dataBody),
+		Status:      resp.StatusCode,
+		SentBody:    string(C.GoString(bodyRaw)),
+		SentUrl:     string(C.GoString(urlRaw)),
+		SentHeaders: headerList,
 	})
 	if err != nil {
 		return C.CString("error3")
@@ -89,35 +138,22 @@ func postRequest(urlRaw *C.char, headersRaw *C.char, bodyRaw *C.char, proxyRaw *
 
 //export putRequest
 func putRequest(urlRaw *C.char, headersRaw *C.char, proxyRaw *C.char) *C.char {
+	url := string(C.GoString(urlRaw))
+	headers := string(C.GoString(headersRaw))
+	proxy := string(C.GoString(proxyRaw))
 
-	return rr("PUT",urlRaw,headersRaw,proxyRaw);
-}
-
-func getRequest(urlRaw *C.char, headersRaw *C.char, proxyRaw *C.char) *C.char {
-
-	return rr("GET",urlRaw,headersRaw,proxyRaw);
-}
-
-func rr(string ty,urlRaw *C.char, headersRaw *C.char, proxyRaw *C.char) *C.char {
-	var wg sync.WaitGroup
-	go url := string(C.GoString(urlRaw))
-	go headers := string(C.GoString(headersRaw))
-	go proxy := string(C.GoString(proxyRaw))
-
-	go req, err := http.NewRequest(ty, url, nil)
+	req, err := http.NewRequest("PUT", url, nil)
 	if err != nil {
-		return C.CString("get error")
+		return C.CString("put error")
 	}
-	
+
 	headerList := strings.Split(headers, "=|=|=")
-	wg.Add(len(headerList))
 	for i := 0; i < len(headerList); i++ {
-		//headerAA := strings.Split(headerList[i], ",,")
-		//req.Header.Set(headerAA[0], headerAA[1])
+		headerAA := strings.Split(headerList[i], ",,")
+		req.Header.Set(headerAA[0], headerAA[1])
 		//req.Header[headerAA[0]] = []string{headerAA[1]}
-		go headSet(req,headerList)
 	}
-	wg.Wait()
+
 	client, err := cclient.NewClient(tls.HelloChrome_Auto, proxy)
 	if err != nil {
 		return C.CString("newClient error")
