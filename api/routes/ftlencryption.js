7519626fcfecc98bb23086937aae99e31a829aa7
Drizzs <drizzs@hotmail.com>
2021-01-26 09:54:00 -0500

    updates to web api

--- api/routes/ftlencryption.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const adyenEncrypt = require('');
-const router = express.Router();
-
-router.get('/:num/:cvv/:month/:year', (req,res,next) =>{
-    res.status(200).json(getResponseJsonFromRequestJson(req))
-})
-
--- api/server.js
+++
@@ -1,8 +0,0 @@
-const http= require('http');
-const app = require("../api")
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(PORT);
